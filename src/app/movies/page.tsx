38f0ff8559bf3e577a9824ac1db161a99286ff09
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2025-09-29 21:01:39 +0000

    Home page

---
+++ src/app/movies/page.tsx
@@ -0,0 +1,10 @@
+export default function Movies(){
+    return <>
+        <div className="flex justify-center ">
+            <input type="text" placeholder="Search for movies..." className="border m-1 p-2 rounded-md" /> <button className="m-1 p-2 rounded-md text-white bg-blue-500">Search</button>
+        </div>
+        <div>
+            
+        </div>
+    </>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
 export default function Home(){
     return <div>
-        <h1>
-            Benedict
-        </h1>
-
-        <br />
-        <h3>
-            Asiedu
-        </h3>
+        
     </div>
 }
