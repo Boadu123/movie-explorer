7ed61588beae0934aaf3b60e1e20688f6475d77e
Benedict Boadu-Boateng <bboaduboateng2000@gmail.com>
2025-09-30 10:55:58 +0000

    Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
+import Link from "next/link"
+
 export default function Home(){
     return <div>
-        
+        <Link href ="/movies" className="p-4 m-6 rounded-md bg-blue-500 text-white">Enjoy some Movies</Link>
     </div>
 }
